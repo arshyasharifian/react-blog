7a7ea71664d93f27cd082523d58e7963c7528685
Arshya Sharifian <arshyasharifian@gmail.com>
2022-04-04 21:40:52 -0700

    moving files around

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
-import Navbar from './components/Navbar';
-import Home from './components/Home';
-import Create from './components/Create';
-import BlogDetails from './components/BlogDetails';
-import NotFound from './components/NotFound';
+import Navbar from './Navbar';
+import Home from './Home';
+import Create from './Create';
+import BlogDetails from './BlogDetails';
+import NotFound from './NotFound';
 import { BrowserRouter as Router, Route, Routes} from 'react-router-dom'; 
 
 function App() {
